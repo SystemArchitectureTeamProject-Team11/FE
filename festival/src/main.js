4932d0add6e935ac38013998bd859aae5b43c2fe
MinhoJJang <ahchjang@naver.com>
2023-11-15 15:34:51 +0900

    jquery script 추가

--- festival/src/main.js
+++ festival/src/main.js
@@ -1,14 +1,17 @@
 require("./main.css");
-import "../js/api.js";
+
 import "../js/bootstrap.min.js";
-import "../js/easing.js";
-import "../js/genres_api.js";
+
 import "../js/jquery-2.1.4.min.js";
 import "../js/jquery.flexslider.js";
 import "../js/jquery.magnific-popup.js";
 import "../js/jquery.slidey.js";
+import "../js/easing.js";
+
 import "../js/move-top.js";
 import "../js/owl.carousel.js";
 import "../js/simplePlayer.js";
-import "./css/index.css";
 import "../js/data.js";
+import "../js/api.js";
+import "../js/genres_api.js";
+import "./css/index.css";
