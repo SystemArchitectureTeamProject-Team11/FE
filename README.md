707b3e9a4069349ad467b633802619e143e4e166
MinhoJJang <ahchjang@naver.com>
2023-11-15 14:32:04 +0900

    modify folder structure

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 [Figma](https://www.figma.com/file/rpMLepVcAmtfArTp1z6xgJ/Untitled?type=design&node-id=0%3A1&mode=design&t=liIRlM1UxxfoYDM1-1)
 
 <img width="1280" alt="image" src="https://github.com/SystemArchitectureTeamProject-Team11/FE/assets/112792949/51e3077b-49d0-4ba7-a133-0fdea672d419">
-
---
+++ festival/src/main.js
@@ -0,0 +1,13 @@
+require("./main.css");
+import "../js/api.js";
+import "../js/bootstrap.min.js";
+import "../js/easing.js";
+import "../js/genres_api.js";
+import "../js/jquery-2.1.4.min.js";
+import "../js/jquery.flexslider.js";
+import "../js/jquery.magnific-popup.js";
+import "../js/jquery.slidey.js";
+import "../js/move-top.js";
+import "./owl.carousel.js";
+import "./simplePlayer.js";
+import "./css/index.css";
